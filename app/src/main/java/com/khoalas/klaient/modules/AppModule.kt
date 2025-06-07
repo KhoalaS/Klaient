8ab2c0de424d9cb24c90d69ac7c16964d3839117
KhoalaS <khoalekappa@gmail.com>
2025-06-07 23:30:22 +0200

    repeat mode all

--- app/src/main/java/com/khoalas/klaient/modules/AppModule.kt
+++ app/src/main/java/com/khoalas/klaient/modules/AppModule.kt
@@ -1,5 +1,7 @@
 package com.khoalas.klaient.modules
 
+import android.content.Context
+import androidx.media3.common.Player.REPEAT_MODE_ALL
 import androidx.media3.exoplayer.ExoPlayer
 import com.khoalas.klaient.services.Route
 import com.khoalas.klaient.viewmodel.FeedViewModel
@@ -7,11 +9,17 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
+fun getPlayer(context: Context) : ExoPlayer {
+    val player = ExoPlayer.Builder(context).build()
+    player.repeatMode = REPEAT_MODE_ALL
+    return player
+}
+
 val appModule = module {
     viewModel { (route: Route) ->
         FeedViewModel(get(), get(), route)
     }
     single {
-        ExoPlayer.Builder(androidContext()).build()
+        getPlayer(androidContext())
     }
 }
