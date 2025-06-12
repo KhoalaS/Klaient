6215ff92c3b1c9f45996644fe86f3090e964f288
KhoalaS <khoalekappa@gmail.com>
2025-06-12 19:24:18 +0200

    remove system bar hiding

--- app/src/main/java/com/khoalas/klaient/screens/FullscreenVideoScreen.kt
+++ app/src/main/java/com/khoalas/klaient/screens/FullscreenVideoScreen.kt
@@ -1,6 +1,6 @@
 package com.khoalas.klaient.screens
 
-import androidx.activity.compose.LocalActivity
+import androidx.activity.compose.BackHandler
 import androidx.annotation.OptIn
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
@@ -8,31 +8,46 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Download
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.compose.state.rememberPresentationState
 import com.khoalas.klaient.services.DownloadService
 import com.khoalas.klaient.ui.noRippleClickable
 import com.khoalas.klaient.ui.player.Player
 import com.khoalas.klaient.ui.player.state.rememberPlayerControlVisibility
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.koin.compose.koinInject
 import java.util.UUID
 
 @OptIn(UnstableApi::class)
+@ExperimentalMaterial3Api
 @Composable
 fun FullscreenVideoScreen(
     modifier: Modifier,
@@ -40,11 +55,35 @@ fun FullscreenVideoScreen(
     downloadUri: String?,
     onExit: () -> Unit
 ) {
+    var shouldShowPlayer by remember { mutableStateOf(true) }
+    val presentationState = rememberPresentationState(player)
+
     val controlState = rememberPlayerControlVisibility()
     val downloadService = koinInject<DownloadService>()
     val coroutineScope = rememberCoroutineScope()
 
-    HideSystemBars()
+    var position by remember { mutableLongStateOf(0L) }
+    val duration = player.duration.coerceAtLeast(0L)
+    var buffer by remember { mutableIntStateOf(player.bufferedPercentage) }
+
+    var currentSize = presentationState.videoSizeDp
+    val density = LocalDensity.current
+
+// Handle system back press to clean up first
+    BackHandler {
+        shouldShowPlayer = false
+        player.stop()
+        onExit()
+    }
+
+    // Polling coroutine
+    LaunchedEffect(Unit) {
+        while (true) {
+            position = player.currentPosition
+            buffer = player.bufferedPercentage
+            delay(500L)
+        }
+    }
 
     Box(
         modifier
@@ -54,16 +93,29 @@ fun FullscreenVideoScreen(
             })
     )
     {
-        Player(
-            modifier = Modifier
-                .fillMaxSize(),
-            player = player,
-            onFullscreen = {
-            },
-            showControls = controlState.showControls,
-            onUserInteraction = controlState.onUserInteraction,
-            toggleControls = controlState.toggleControls
-        )
+        if (shouldShowPlayer) {
+            Player(
+                modifier = Modifier
+                    .fillMaxSize(),
+                player = player,
+                onFullscreen = {
+                },
+                showControls = controlState.showControls,
+                onUserInteraction = controlState.onUserInteraction,
+                toggleControls = controlState.toggleControls
+            )
+        } else {
+            if (currentSize != null) {
+                with(density) {
+                    Box(
+                        modifier = Modifier.size(
+                            currentSize.width.toDp(),
+                            currentSize.height.toDp(),
+                        )
+                    )
+                }
+            }
+        }
         AnimatedVisibility(
             visible = controlState.showControls,
             enter = fadeIn(),
@@ -73,7 +125,11 @@ fun FullscreenVideoScreen(
                 modifier = Modifier.fillMaxSize()
             ) {
                 IconButton(
-                    onClick = { onExit() },
+                    onClick = {
+                        shouldShowPlayer = false
+                        player.stop()
+                        onExit()
+                    },
                     modifier = Modifier.align(Alignment.TopStart)
                 ) {
                     Icon(
@@ -82,7 +138,6 @@ fun FullscreenVideoScreen(
                         tint = Color.White
                     )
                 }
-
                 IconButton(
                     onClick = {
                         coroutineScope.launch {
@@ -102,27 +157,29 @@ fun FullscreenVideoScreen(
                         tint = Color.White
                     )
                 }
+                Box(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .fillMaxWidth()
+                        .padding(bottom = 72.dp, start = 12.dp, end = 12.dp)
+                ) {
+                    Slider(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(2.dp),
+                        value = position.toFloat(),
+                        valueRange = 0f..duration.toFloat(),
+                        onValueChange = {
+                            player.seekTo(it.toLong().coerceAtLeast(0L))
+                            position = it.toLong().coerceAtLeast(0L)
+                            controlState.onUserInteraction()
+                        },
+                        track = {
+                            SliderDefaults.Track(sliderState = it, modifier = Modifier.height(4.dp))
+                        }
+                    )
+                }
             }
-        }
-    }
-}
-
-@Composable
-fun HideSystemBars() {
-    val activity = LocalActivity.current ?: return
-
-    val window = activity.window
-    WindowCompat.setDecorFitsSystemWindows(window, false)
-    val controller = WindowInsetsControllerCompat(window, window.decorView)
-
-    controller.systemBarsBehavior =
-        WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-
-    controller.hide(WindowInsetsCompat.Type.systemBars())
-
-    DisposableEffect(Unit) {
-        onDispose {
-            controller.show(WindowInsetsCompat.Type.systemBars())
         }
     }
 }
